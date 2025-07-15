0b86d4b07cef9892fa199f0ba9736448f987427a
Satish Chandra <satish.chandra@cashfree.com>
2025-07-15 14:52:13 +0530

    Add zup formula

--- Formula/zup.rb
+++ Formula/zup.rb
@@ -1,9 +1,9 @@
 class Zup < Formula
   desc "A fast, customizable CLI built with Cobra to automate local development environment setup — from cloning repos to installing tools and running services."
   homepage "https://github.com/devglyph1/zup"
-  url "https://github.com/devglyph1/zup/releases/download/v0.1.0/zup.tar.gz"
-  sha256 "1a8f30860bdce8eea74496dffcee85d25fe83b6a6cd8b743a3dc3804e1129515"
-  version "0.1.0"
+  url "https://github.com/devglyph1/zup/releases/download/v0.1.1/zup.tar.gz"
+  sha256 "b1124c9c226ba9b24948118f61686e0c925d53a56081ec4e9c36abb55af17728"
+  version "0.1.1"
 
   def install
     bin.install "zup"
