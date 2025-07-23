0af42f5a0a31be31ef523eac3d655f454b0e6bda
Satish Chandra <satish.chandra@cashfree.com>
2025-07-23 15:35:00 +0530

    added new release

--- Formula/zup.rb
+++ Formula/zup.rb
@@ -1,7 +1,7 @@
 class Zup < Formula
   desc "A fast, customizable CLI built with Cobra to automate local development environment setup — from cloning repos to installing tools and running services."
   homepage "https://github.com/devglyph1/homebrew-zup"
-  url "https://github.com/devglyph1/homebrew-zup/releases/download/v0.1.1/zup.tar.gz"
+  url "https://github.com/devglyph1/homebrew-zup/releases/download/v0.1.0/zup.tar.gz"
   sha256 "22fbae5b510a538d2066f755cb8207914d3d95a7e426c0e304fedc2cd2383ce1"
   version "0.1.2"
 
