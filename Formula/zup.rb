b1d8b5332f338622e4ae7a4b120473ffc0c07214
Satish Chandra <satish.chandra@cashfree.com>
2025-07-31 14:28:13 +0530

    added auto pick path feature based on current directory

--- Formula/zup.rb
+++ Formula/zup.rb
@@ -1,9 +1,9 @@
 class Zup < Formula
   desc "A fast, customizable CLI built with Cobra to automate local development environment setup — from cloning repos to installing tools and running services."
   homepage "https://github.com/devglyph1/homebrew-zup"
-  url "https://github.com/devglyph1/homebrew-zup/releases/download/v0.1.0/zup.tar.gz"
-  sha256 "22fbae5b510a538d2066f755cb8207914d3d95a7e426c0e304fedc2cd2383ce1"
-  version "0.1.2"
+  url "https://github.com/devglyph1/homebrew-zup/releases/download/v0.1.1/zup.tar.gz"
+  sha256 "6a4d01254bf63ddf5e19e08abffcc5251ecf80953e3aa0087c1747678313fefa"
+  version "0.1.3"
 
   def install
     bin.install "zup"
