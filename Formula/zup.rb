1b3f42641dd241bacf2791289eed1925b08c81fc
satishChadive <satish.chandra@cashfree.com>
2025-08-07 17:40:31 +0530

    fixed one bug

--- Formula/zup.rb
+++ Formula/zup.rb
@@ -1,9 +1,9 @@
 class Zup < Formula
   desc "A fast, customizable CLI built with Cobra to automate local development environment setup — from cloning repos to installing tools and running services."
   homepage "https://github.com/devglyph1/homebrew-zup"
-  url "https://github.com/devglyph1/homebrew-zup/releases/download/v0.1.6/zup.tar.gz"
-  sha256 "c76cabb6377577eb8124f209c3c65ef3215f216613c1d24c849990505962224e"
-  version "0.1.6"
+  url "https://github.com/devglyph1/homebrew-zup/releases/download/v0.1.7/zup.tar.gz"
+  sha256 "ed218fa90e8adeecfe3a888f70393ba985d8bd4754bb664f95bebece097e9c73"
+  version "0.1.7"
 
   def install
     bin.install "zup"
