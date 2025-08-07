4adbfbbfdd6196dcca60a3c926e31e0b09368ee4
satishCashFree <139214853+satishCashFree@users.noreply.github.com>
2025-08-07 17:15:54 +0530

    added missing files (#7)

--- Formula/zup.rb
+++ Formula/zup.rb
@@ -2,7 +2,7 @@ class Zup < Formula
   desc "A fast, customizable CLI built with Cobra to automate local development environment setup — from cloning repos to installing tools and running services."
   homepage "https://github.com/devglyph1/homebrew-zup"
   url "https://github.com/devglyph1/homebrew-zup/releases/download/v0.1.6/zup.tar.gz"
-  sha256 "699612b1e787a47ebb94d48a5789315925d12f4db718d9e1316a0c0865725538"
+  sha256 "c76cabb6377577eb8124f209c3c65ef3215f216613c1d24c849990505962224e"
   version "0.1.6"
 
   def install
